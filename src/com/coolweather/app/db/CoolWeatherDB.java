92f292cb59fffb3b74058cdc011cada13386766d
lmm <lmm@gmail.com>
2016-07-04 17:21:33 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/com/coolweather/app/db/CoolWeatherDB.java
@@ -0,0 +1,124 @@
+package com.coolweather.app.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class CoolWeatherDB {
+	
+	public static final String DB_NAME="cool_weather";
+	public static final int DB_VERSION=1;
+	private SQLiteDatabase sqLiteDatabase;
+	private static CoolWeatherDB coolWeatherDB;
+	
+	
+	private CoolWeatherDB(Context context){
+		CoolWeatherOpenHelper coolWeatherOpenHelper=new CoolWeatherOpenHelper(context, DB_NAME, null, DB_VERSION);
+		sqLiteDatabase=coolWeatherOpenHelper.getWritableDatabase();
+	}
+	
+	public synchronized static CoolWeatherDB getInstance(Context context){
+		if(coolWeatherDB==null){
+			coolWeatherDB=new CoolWeatherDB(context);
+		}
+		return coolWeatherDB;
+	}
+	
+	public void saveProvince(Province province){
+		ContentValues contentValues=new ContentValues();
+		contentValues.put("province_name", province.getProvinceName());
+		contentValues.put("province_code", province.getProvinceCode());
+		sqLiteDatabase.insert("Province", null, contentValues);
+	}
+	
+	public List<Province> loadProvinces(){
+		List<Province> list=new ArrayList<Province>();
+		Cursor cursor=sqLiteDatabase.query("Province", null, null, null, null, null, null);
+		if(cursor.moveToFirst()){
+			do{
+				Province province=new Province();
+				province.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
+				province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+				list.add(province);
+			}while(cursor.moveToNext());
+		}
+		return list;
+	}
+	
+	public void saveCity(City city) {
+		if (city != null) {
+			ContentValues values = new ContentValues();
+			values.put("city_name", city.getCityName());
+			values.put("city_code", city.getCityCode());
+			values.put("province_id", city.getProvinceId());
+			sqLiteDatabase.insert("City", null, values);
+		}
+	}
+
+	/**
+	 * 从数据库读取某省下所有的城市信息。
+	 */
+	public List<City> loadCities(int provinceId) {
+		List<City> list = new ArrayList<City>();
+		Cursor cursor = sqLiteDatabase.query("City", null, "province_id = ?",
+				new String[] { String.valueOf(provinceId) }, null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				City city = new City();
+				city.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				city.setCityName(cursor.getString(cursor
+						.getColumnIndex("city_name")));
+				city.setCityCode(cursor.getString(cursor
+						.getColumnIndex("city_code")));
+				city.setProvinceId(provinceId);
+				list.add(city);
+			} while (cursor.moveToNext());
+		}
+		return list;
+	}
+
+	/**
+	 * 将County实例存储到数据库。
+	 */
+	public void saveCounty(County county) {
+		if (county != null) {
+			ContentValues values = new ContentValues();
+			values.put("county_name", county.getCountyName());
+			values.put("county_code", county.getCountyCode());
+			values.put("city_id", county.getCityId());
+			sqLiteDatabase.insert("County", null, values);
+		}
+	}
+
+	/**
+	 * 从数据库读取某城市下所有的县信息。
+	 */
+	public List<County> loadCounties(int cityId) {
+		List<County> list = new ArrayList<County>();
+		Cursor cursor = sqLiteDatabase.query("County", null, "city_id = ?",
+				new String[] { String.valueOf(cityId) }, null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				County county = new County();
+				county.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				county.setCountyName(cursor.getString(cursor
+						.getColumnIndex("county_name")));
+				county.setCountyCode(cursor.getString(cursor
+						.getColumnIndex("county_code")));
+				county.setCityId(cityId);
+				list.add(county);
+			} while (cursor.moveToNext());
+		}
+		return list;
+	}
+	
+}
